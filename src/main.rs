a46103d0d0f9f0dfd8145590d133e193d85e6a54
Brandon Tripp <brandongtripp@gmail.com>
2024-01-18 18:21:50 -0700

    Setup for cli scaffolding and test of empty args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use chrono::{DateTime, Utc};
+use serde::{Serialize, Deserialize};
+use serde_yaml;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(subcommand)]
+    command: Option<Command>
+} 
+
+#[derive(StructOpt)]
+enum Command {
+    Tasks,
+    Snippets,
+    Timers,
 }
+
+fn main() {
+    let cli = Cli::from_args();
+
+    match &cli.command {
+        Some(Command::Tasks) => {
+        },
+        Some(Command::Snippets) => {
+        },
+        Some(Command::Timers) => {
+        },
+        None => {
+            println!("No command passed");
+        }, 
+    } 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cli_no_command() {
+        let cli = Cli::from_args();
+        assert!(cli.command.is_none()); 
+    }
+
+} 
