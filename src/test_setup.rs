33e19d4e3c6a2293838220ff8f9644c3824a5122
Brandon Tripp <brandongtripp@gmail.com>
2024-01-19 09:14:23 -0700

    Write mock yaml file creation for tests

--- src/test_setup.rs
+++ src/test_setup.rs
@@ -4,3 +4,14 @@ pub fn setup() {
     let mut cmd = Command::cargo_bin("task-manager").unwrap();
     cmd.assert().success(); // build binary
 } 
+
+pub fn create_test_config() -> String {
+    let s = r#"
+        path: tests/data/config.yml
+        last_updated: 12345
+    "#;
+
+    s.to_string()
+} 
+
+
