ec64397e62c78d53530e2e3243ca674f2d874903
Brandon Tripp <brandongtripp@gmail.com>
2024-01-19 09:12:43 -0700

    WIP: Config setup and basic tests

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,59 @@
-pub struct Config {
+use std::path::Path;
 
+pub struct Config {
+    pub path: Option<String>, 
+    pub last_updated: u64,
 } 
 
 impl Config {
+    pub fn new() -> Self {
+        Self {
+            path: None, 
+            last_updated: 0
+        }
+    } 
+
+    pub fn load(&mut self, path: &str) -> Result<(), ConfigError> {
+        // Check if file exists
+        if !Path::new(path).exists() {
+            return Err(ConfigError::InvalidPath)
+        } 
+
+        Ok(())
+    }
 } 
 
-#[test]
+#[derive(Debug)]
+pub enum ConfigError {
+    InvalidPath, 
+    IoError(std::io::Error),
+    YamlError(serde_yaml::Error)
+} 
+
+impl std::fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidPath => write!(f, "Invalid path provided"),
+            Self::IoError(err) => write!(f, "I/O error: {}", err),
+            Self::YamlError(err) => write!(f, "Error parsing YAML: {}", err),
+        } 
+    }
+} 
+
+impl std::error::Error for ConfigError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::IoError(err) => Some(err),
+            Self::YamlError(err) => Some(err),
+            _ => None,
+        } 
+    }
+} 
+
+#[cfg(test)]
 mod tests {
+    use crate::test_setup::create_test_config;
+
     use super::*;
 
     #[test]
@@ -28,8 +75,15 @@ mod tests {
         // Assert error 
         assert!(result.is_err());
 
+        // Generate valid yaml string 
+        let yaml = create_test_config();
+
+        // Write string to temp file 
+        let tmp_file = "tmp.yml";
+        std::fs::write(tmp_file, yaml);
+
         // Load Valid Path 
-        let result = config.load("tests/data/config.yml");
+        let mut result = config.load("tests/data/config.yml");
 
         // Assert Success
         assert!(result.is_ok());
@@ -38,5 +92,7 @@ mod tests {
         // Assert fields populated 
         assert_eq!(config.path, Some("tests/data/config.yml".into()));
         assert!(config.last_updated > 0);
+
+        std::fs::remove_file(tmp_file);
     } 
 } 
