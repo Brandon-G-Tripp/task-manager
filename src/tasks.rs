e6c4dd3270daa60c3f6c18a6d44e83d6c80b2cd4
Brandon Tripp <brandongtripp@gmail.com>
2024-01-20 15:03:54 -0700

    Create basic test for new function, task struct and task impl for constructor

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,3 +1,47 @@
+use chrono::{DateTime, Utc};
+
+pub struct Task {
+    id: i32,
+    name: String,
+    description: String,
+    due_date: DateTime<Utc>,
+} 
+
+impl Task {
+    pub fn new(id: i32, name: String, description: String, due_date: DateTime<Utc>) -> Self {
+        Self {
+            id,
+            name,
+            description,
+            due_date
+        } 
+    } 
+}
+
 pub fn run() {
     println!("Tasks placeholder");
+} 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_task() {
+        let due_date = {
+            let datetime = DateTime::parse_from_str("2023-03-01T12:00:00Z", "%+").unwrap();
+            datetime.into()
+            
+        };
+        let task = Task::new(
+            1, 
+            "My Task".to_string(), 
+            "Description".to_string(), 
+            due_date
+        );
+
+        assert_eq!(task.id, 1);
+        assert_eq!(task.name, "My Task");
+
+    } 
 } 
