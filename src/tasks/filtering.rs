ee0beff45c1417e6e70a14cc074cccda0097d4cd
Brandon Tripp <brandongtripp@gmail.com>
2024-01-27 09:25:12 -0700

    Correct of filtering logic for testing if a task is due today and within the next week

--- src/tasks/filtering.rs
+++ src/tasks/filtering.rs
@@ -14,21 +14,30 @@ impl DueFilter {
     pub fn filter(self, tasks: &[Task]) -> Vec<Task> {
         match self {
             DueFilter::PastDue => {
+                let today = Utc::now().naive_utc();
                 tasks.iter()
-                    .filter(|t| t.due_date.naive_utc() < Utc::now().naive_utc())
+                    .filter(|t| t.due_date.naive_utc() < today )
                     .cloned()
                     .collect()
             },
             DueFilter::DueToday => {
+                let today_start = Utc::now().naive_utc().date().and_hms_opt(0, 0, 0).unwrap();
+                let today_end = Utc::now().naive_utc().date().and_hms_opt(23, 59, 59).unwrap();
+
                 tasks.iter()
-                    .filter(|t| t.due_date.naive_utc() == Utc::now().naive_utc())
+                    .filter(|t| {
+                        t.due_date.naive_utc() >= today_start && t.due_date.naive_utc() <= today_end
+                    })
                     .cloned()
                     .collect()
             },
             DueFilter::DueThisWeek => {
+                let start = Utc::now().naive_utc(); 
+                let end = start + chrono::Duration::days(7);
                 tasks.iter()
                     .filter(|t| {
-                        t.due_date.naive_utc() <= Utc::now().naive_utc() + chrono::Duration::days(7)
+                        let due_date = t.due_date.naive_utc();  
+                        due_date >= start && due_date <= end
                     })
                     .cloned()
                     .collect()
