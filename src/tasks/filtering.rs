3dae546c778285df5fd191bfdccacc94a9051819
Brandon Tripp <brandongtripp@gmail.com>
2024-01-27 09:47:00 -0700

    Tests for the new completed filter, all, complete and incomplete

--- src/tasks/filtering.rs
+++ src/tasks/filtering.rs
@@ -52,7 +52,9 @@ mod tests {
     use chrono::{DateTime, Utc};
 
     use super::*;
-    use crate::tests_common::create_tasks;
+    use crate::tests_common::{create_tasks, create_tasks_completion};
+
+    // Tests for DueFilter
 
     #[test]
     fn filter_past_due() {
@@ -93,4 +95,36 @@ mod tests {
         assert_eq!(results[3].id, 4);
         assert_eq!(results[4].id, 5);
     }  
+
+
+    // Tests for completed filter
+    #[test]
+    fn returns_all_tasks_when_all_filter() {
+        let tasks = create_tasks_completion();
+
+        let result = CompletionFilter::All.filter(&tasks);
+
+        assert_eq!(result.len(), 3);
+    } 
+
+    #[test]
+    fn returns_only_completed_tasks() {
+        let tasks = create_tasks_completion();
+
+        let result = CompletionFilter::Complete.filter(&tasks);
+
+        assert_eq!(result.len(), 2);
+        assert!(result[0].completed);
+        assert!(result[1].completed);
+    } 
+
+    #[test]
+    fn returns_only_incomplete_tasks() {
+        let tasks = create_tasks_completion();
+
+        let result = CompletionFilter::Complete.filter(&tasks);
+
+        assert_eq!(result.len(), 1);
+        assert!(!result[0].completed);
+    } 
 } 
