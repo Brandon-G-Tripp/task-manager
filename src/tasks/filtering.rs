5e731f440c2599aca2958d861d4ce49d12a59a7a
Brandon Tripp <brandongtripp@gmail.com>
2024-01-27 07:33:07 -0700

    Impl for DueDate filter enum that is called from the crud.rs

--- src/tasks/filtering.rs
+++ src/tasks/filtering.rs
@@ -1,6 +1,39 @@
+use chrono::Utc;
+
+use super::Task;
+
 pub enum DueFilter {
     PastDue, 
     DueToday, 
     DueThisWeek, 
     All
 } 
+
+
+impl DueFilter {
+    pub fn filter(self, tasks: &[Task]) -> Vec<Task> {
+        match self {
+            DueFilter::PastDue => {
+                tasks.iter()
+                    .filter(|t| t.due_date.naive_utc() < Utc::now().naive_utc())
+                    .cloned()
+                    .collect()
+            },
+            DueFilter::DueToday => {
+                tasks.iter()
+                    .filter(|t| t.due_date.naive_utc() == Utc::now().naive_utc())
+                    .cloned()
+                    .collect()
+            },
+            DueFilter::DueThisWeek => {
+                tasks.iter()
+                    .filter(|t| {
+                        t.due_date.naive_utc() <= Utc::now().naive_utc() + chrono::Duration::days(7)
+                    })
+                    .cloned()
+                    .collect()
+            }, 
+            DueFilter::All => tasks.to_vec()
+        } 
+    } 
+}
