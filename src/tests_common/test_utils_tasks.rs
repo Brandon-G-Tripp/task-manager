7e066b62653a3b3bc027e686aa612e7e118da2fd
Brandon Tripp <brandongtripp@gmail.com>
2024-01-27 09:43:16 -0700

    Create tasks for completed tests

--- src/tests_common/test_utils_tasks.rs
+++ src/tests_common/test_utils_tasks.rs
@@ -1,6 +1,6 @@
 use chrono::Utc;
 
-use crate::tasks::Task;
+use crate::tasks::{Task, Tasks};
 
 pub fn create_tasks() -> Vec<Task> {
     vec![
@@ -11,3 +11,17 @@ pub fn create_tasks() -> Vec<Task> {
         Task::new(5, "Task 5".to_string(), "Description".to_string(), (Utc::now() + chrono::Duration::days(2)).to_string())
     ]
 }
+
+pub fn create_tasks_completion() -> Tasks {
+    // Setup 
+    let mut tasks = Tasks::new();
+    let due_date = Utc::now().to_string();
+    tasks.add_task("Task 1".to_string(), "Text for task1".to_string(), due_date); 
+    tasks.add_task("Task 2".to_string(), "Description".to_string(), (Utc::now() + chrono::Duration::days(3)).to_string());
+    tasks.add_task("Task 3".to_string(), "Description".to_string(), (Utc::now() + chrono::Duration::days(2)).to_string());
+
+    tasks.complete_task(1);
+    tasks.complete_task(2);
+
+    tasks
+} 
