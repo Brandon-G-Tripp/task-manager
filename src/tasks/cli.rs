5d695d250da9569e73458f83c07c47557704e3bd
Brandon Tripp <brandongtripp@gmail.com>
2024-01-31 14:35:07 -0700

    Two tests add that now pass for the Add and List commands

--- src/tasks/cli.rs
+++ src/tasks/cli.rs
@@ -69,7 +69,7 @@ mod tests{
         let cmd = TaskCommand::Add {
             name: "Task 1".to_string(),
             description: "Description 1".to_string(), 
-            due_date: "2023-01-01".to_string()
+            due_date: "2023-03-01T12:00:00Z".to_string()
         };
 
         run(&mut tasks, &cmd);
@@ -77,15 +77,18 @@ mod tests{
         assert_eq!(tasks.tasks.len(), 1);
         assert_eq!(tasks.tasks[0].name, "Task 1");
         assert_eq!(tasks.tasks[0].description, "Description 1");
-        assert_eq!(tasks.tasks[0].due_date.to_string(), "2023-01-01");
+        assert_eq!(tasks.tasks[0].due_date.to_string(), "2023-03-01 12:00:00 UTC");
     }
 
     #[test]
     fn test_list_command() {
         let mut tasks = Tasks::new();
-        tasks.add_task("Task 1".to_string(), "".to_string(), "".to_string());
+        tasks.add_task("Task 1".to_string(), "".to_string(), "2023-03-01T12:00:00Z".to_string());
+
+        let cmd = TaskCommand::List { due: None, status: None };
         
         let mut writer = Vec::new();
+        run(&mut tasks, &cmd);
         tasks.list_tasks(&mut writer, &None, &None);
 
         let output = String::from_utf8(writer).unwrap();
