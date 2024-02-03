9a9b3fd1f0443bd9c653af54c9f84ef224b1e976
Brandon Tripp <brandongtripp@gmail.com>
2024-02-02 17:22:26 -0700

    Modify create tasks task for due today

--- src/tests_common/test_utils_tasks.rs
+++ src/tests_common/test_utils_tasks.rs
@@ -6,7 +6,7 @@ pub fn create_tasks() -> Vec<Task> {
     vec![
         Task::new(1, "Task 1".to_string(), "Description".to_string(), (Utc::now() - chrono::Duration::days(3)).to_string()),
         Task::new(2, "Task 2".to_string(), "Description".to_string(), (Utc::now() - chrono::Duration::days(2)).to_string()),
-        Task::new(3, "Task 3".to_string(), "Description".to_string(), (Utc::now() + chrono::Duration::hours(1)).to_string()),
+        Task::new(3, "Task 3".to_string(), "Description".to_string(), (Utc::now() + chrono::Duration::hours(2)).to_string()),
         Task::new(4, "Task 4".to_string(), "Description".to_string(), (Utc::now() + chrono::Duration::days(1)).to_string()),
         Task::new(5, "Task 5".to_string(), "Description".to_string(), (Utc::now() + chrono::Duration::days(2)).to_string())
     ]
