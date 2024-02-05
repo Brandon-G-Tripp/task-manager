95dae99d6c72b9825dc33c969218033637b187a9
Brandon Tripp <brandongtripp@gmail.com>
2024-02-05 10:53:36 -0700

    Handle invalid values for the due date and completed fields if they are present in input

--- src/tasks/update.rs
+++ src/tasks/update.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
+use regex::Regex;
 
-use crate::tasks::TaskError;
+use super::TaskError;
 
 #[derive(Debug)]
 pub enum TaskCommandUpdateArgs {
@@ -41,28 +42,46 @@ impl FromStr for UpdateFields {
     }
 } 
 
-pub fn parse_update_fields(update_args: &str) -> UpdateFields {
+pub fn parse_update_fields(update_args: &str) -> Result<UpdateFields, TaskError>{
     let mut update_fields = UpdateFields::default();
 
     for pair in update_args.split(", ") {
         let kv: Vec<_> = pair.splitn(2, ':').collect();
 
-        if kv.len() == 2 {
-            let key = kv[0];
-            let value = kv[1];
+        if kv.len() != 2 {
+            continue
+        }
 
-            match key {
-                "name" => update_fields.name = Some(value.to_string()),
-                "description" => update_fields.description = Some(value.to_string()),
-                "due_date" => update_fields.due_date = Some(value.to_string()),
-                "completed" => update_fields.completed = Some(value.to_string()),
-                _ => println!("Unknown field key: {}", key),
-            }
-        } 
+        let key = kv[0];
+        let value = kv[1];
+
+        match key {
+            "name" => update_fields.name = Some(value.to_string()),
+            "description" => update_fields.description = Some(value.to_string()),
+            "due_date" => {
+                if !valid_due_date_format(value) {
+                    return Err(TaskError::InvalidInput("Invalid datetime format for due date".to_string()));
+                } 
+                update_fields.due_date = Some(value.to_string());
+            },
+            "completed" => {
+                if !valid_completed_format(value) {
+                    return Err(TaskError::InvalidInput("Invalid boolean string for completed".to_string()));
+                } 
+                update_fields.completed = Some(value.to_string());
+            },
+            _ => println!("Unknown field key: {}", key),
+        }
     }
 
-    update_fields
+    Ok(update_fields)
 } 
 
+fn valid_due_date_format(value: &str) -> bool {
+    Regex::new(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}")?.is_match(value)
+} 
 
+fn valid_completed_format(value: &str) -> bool {
+    value == "true" || value == "false"
+} 
 
