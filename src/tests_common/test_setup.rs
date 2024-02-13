37de2ed75278770b7615d5d926f44862ea396c1c
Brandon Tripp <brandongtripp@gmail.com>
2024-02-13 10:24:03 -0700

    Removal of unneeded tests in the test common utils

--- src/tests_common/test_setup.rs
+++ src/tests_common/test_setup.rs
@@ -1,8 +1,20 @@
 use assert_cmd::Command;
 
 pub fn setup() {
-    let mut cmd = Command::cargo_bin("task-manager").unwrap();
-    cmd.assert().success(); // build binary
+    // let cmd = Command::cargo_bin("clean")
+    //     .unwrap()
+    //     .current_dir(env!("CARGO_MANIFEST_DIR"));
+    let output = std::process::Command::new("cargo")
+        .arg("clean")
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .output()
+        .unwrap();
+
+    let output = std::process::Command::new("cargo")
+        .arg("build")
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .output()
+        .unwrap();
 } 
 
 pub fn create_test_config() -> String {
@@ -14,25 +26,22 @@ last_updated: 12345
 } 
 
 
-#[test]
-fn test_setup() {
-    crate::tests_common::test_setup::setup();
+#[cfg(test)]
+mod tests {
+    use assert_cmd::Command;
 
-    // make assertions 
-    assert!(Command::cargo_bin("task-manager").is_ok());
-} 
+    #[test]
+    #[should_panic]
+    fn test_setup_fail() {
+        let cmd = Command::cargo_bin("task-manager-invalid").unwrap();
+    } 
 
-#[test]
-#[should_panic]
-fn test_setup_fail() {
-    let cmd = Command::cargo_bin("task-manager-invalid").unwrap();
-} 
+    // #[test]
+    // fn test_after_setup() {
+    //     // Setup will build binary 
+    //     crate::tests_common::test_setup::setup();
 
-#[test]
-fn test_after_setup() {
-    // Setup will build binary 
-    crate::tests_common::test_setup::setup();
-
-    // Now we can assert it exists 
-    assert!(Command::cargo_bin("task-manager").is_ok());
-} 
+    //     // Now we can assert it exists 
+    //     assert!(Command::cargo_bin("task-manager").is_ok());
+    // } 
+}
